598dfacefbdac724c3cdb351ecac3d1bb314b133
Istomine <none@none.com>
2024-11-02 11:32:11 +0100

    cancel button,layout change,refactoring

--- app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
@@ -2,15 +2,24 @@ package ch.heigvd.iict.daa.template
 
 import android.app.DatePickerDialog
 import android.app.Person
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.RadioGroup
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.Group
 import ch.heigvd.iict.daa.labo3.Student
+import ch.heigvd.iict.daa.labo3.Worker
 import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
@@ -18,30 +27,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Références aux éléments
-        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
-        val groupEtudiant = findViewById<Group>(R.id.groupEtudiant)
-        val groupEntreprise = findViewById<Group>(R.id.groupEntreprise)
-
-        // Initialiser les groupes comme invisibles
-
-        groupEtudiant.visibility = View.GONE
-        groupEntreprise.visibility = View.GONE
 
 
-        // Ajouter le listener pour détecter le changement de sélection
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                R.id.etudiant -> {
-                    groupEtudiant.visibility = View.VISIBLE
-                    groupEntreprise.visibility = View.GONE
-                }
-                R.id.employe -> {
-                    groupEtudiant.visibility = View.GONE
-                    groupEntreprise.visibility = View.VISIBLE
-                }
-            }
-        }
+
+
 
         // Référence au champ de date
         val dateAnnivImageButton = findViewById<ImageButton>(R.id.image_button_cake)
@@ -52,12 +41,40 @@ class MainActivity : AppCompatActivity() {
             showDatePickerDialog(dateAnnivEditText)
         }
 
+
+
+        // Spinner
+        nationalitySpinner()
+        SecteurSpinner()
+
+        // Listener
+        buttonListener()
+
+    }
+
+    private fun buttonListener(){
+        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+
+        // Bouton clear
+        findViewById<Button>(R.id.annuler).setOnClickListener{
+            for (i in 0 until mainLayout.childCount) {
+                val view = mainLayout.getChildAt(i)
+
+                when (view) {
+                    is EditText -> view.text.clear()
+                    is Spinner -> view.setSelection(0)
+                    is RadioGroup -> view.clearCheck()
+                }
+            }
+            setGroupVisibility(false,false)
+        }
+
+        // Bouton Ok
         val oKButton = findViewById<Button>(R.id.ok)
-        //Champ sélectioné
-        val selectedRadioButtonId = radioGroup.checkedRadioButtonId
         oKButton.setOnClickListener {
-            if (selectedRadioButtonId == R.id.etudiant) {
-                Student();
+            if ( radioGroup.checkedRadioButtonId == R.id.etudiant) {
+                //Student();
                 val exampleStudent = Student(
                     "Dreher",
                     "Matthias",
@@ -72,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                     "m.dreher@email.com",
                     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                 )
-            }else if (selectedRadioButtonId == R.id.employe) {
+            }else if ( radioGroup.checkedRadioButtonId == R.id.employe) {
 
             }else{
 
@@ -80,9 +97,27 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+        // Radio bouton pour selection etudiant ou employe
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.etudiant -> {
+                    setGroupVisibility(true,false)
+                }
+                R.id.employe -> {
+                    setGroupVisibility(false,true)
+                }
+            }
+        }
+
+
 
     }
 
+    private fun setGroupVisibility(student:Boolean,worker: Boolean){
+     findViewById<Group>(R.id.groupEtudiant).visibility = if(student) VISIBLE else GONE
+     findViewById<Group>(R.id.groupEntreprise).visibility = if(worker) VISIBLE else GONE
+
+    }
 
     private fun showDatePickerDialog(editText: EditText) {
         // Obtenir la date actuelle
@@ -101,5 +136,60 @@ class MainActivity : AppCompatActivity() {
         )
 
         datePickerDialog.show()
+    }
+
+
+    private fun nationalitySpinner(){
+        val spinner = findViewById<Spinner>(R.id.element)
+
+        val nationalities = resources.getStringArray(R.array.nationalities).toMutableList()
+        nationalities.add(0, "Sélectionner")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, nationalities)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                if (position == 0) {
+                    // Si l'utilisateur sélectionne "Sélectionner", on ignore
+                    (view as TextView).setTextColor(Color.GRAY) // Affiche en gris pour donner un effet de "placeholder"
+                } else {
+                    // Actions à réaliser lorsque l'utilisateur choisit une option valide
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Rien à faire ici
+            }
+        }
+    }
+
+    private fun SecteurSpinner(){
+        val spinner = findViewById<Spinner>(R.id.secteur)
+
+        val nationalities = resources.getStringArray(R.array.sectors).toMutableList()
+        nationalities.add(0, "Sélectionner")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, nationalities)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                if (position == 0) {
+                    // Si l'utilisateur sélectionne "Sélectionner", on ignore
+                    (view as TextView).setTextColor(Color.GRAY) // Affiche en gris pour donner un effet de "placeholder"
+                } else {
+                    // Actions à réaliser lorsque l'utilisateur choisit une option valide
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Rien à faire ici
+            }
+        }
     }
 }
